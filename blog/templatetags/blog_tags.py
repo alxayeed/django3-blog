cc6d5f4ffd929e66112695273fe7a7c9267e725d
alxayeed <alxayeed@gmail.com>
2020-09-05 11:58:47 +0600

    custom filter blah is added for markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,6 +2,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 
 # a variable named register(register, nothing else),which is an instance of template.Library(),is used to register customer tags and filters
 register = template.Library()
@@ -29,5 +31,11 @@ def most_commented_posts(count=5):
 # Lets rebuilt the recent_posts() function as a inclusion_tag, it returns render html result (render(template, context))
 @register.inclusion_tag('blog/post/recent_posts.html')  # a template
 def recent_posts(count=5):
-    recent_posts = Post.published.all().order_by('-publish')[:count]
-    return {'recent_posts': recent_posts}  # a context, in
+    recent_posts = Post.published.all().order_by('-updated')[:count]
+    return {'recent_posts': recent_posts}  # a context
+
+
+# CUSTOM FILTER to converts markdown text into normal text
+@register.filter(name='blah')
+def markdown_text(text):
+    return mark_safe(markdown.markdown(text))
