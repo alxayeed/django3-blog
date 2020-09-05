b4e5007ff01001b013009618e51932f76ebcce82
alxayeed <alxayeed@gmail.com>
2020-09-05 14:46:17 +0600

    rss feed added

---
+++ blog/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = "Al's Blog"
+    link = reverse_lazy('blog:list')
+    description = 'New Post of my blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from.feeds import LatestPostsFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     path('tag/<slug:tag_slug>', views.post_list, name='list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_details, name='post_detail'),
-    path('<int:post_id>/share/', views.share, name='share_post')
+    path('<int:post_id>/share/', views.share, name='share_post'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
